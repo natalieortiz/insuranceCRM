a2b40aa34d8ca35e440df572ec3433ad07f6c48e
Natalie Ortiz <ndnatalie@gmail.com>
2016-11-28 13:44:58 -0600

    Adding bootstrap

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,15 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<link rel="stylesheet" href="/css/master.css">
-	<div class="container-fluid">
-		@yield('content')
-		
-	</div>
+	@yield('css')
+	@yield('fonts')
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <body>
+
+	<div class="container-fluid">
+		@yield('content')
+		
+	</div>
 
 </body>
 </html>
--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -4,6 +4,15 @@
 	ICRM
 @stop 
 
+@section('css')
+	<link rel="stylesheet" href="/css/bootstrap.css">
+	<link rel="stylesheet" href="/css/main.css">
+@stop
+
+@section('fonts')
+	<link href="https://fonts.googleapis.com/css?family=Maitree" rel="stylesheet">
+@stop 
+
 @section('content')
 	<h1>hello!</h1>
 @stop
