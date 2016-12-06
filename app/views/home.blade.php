b2794801afc8ba535c6eaee9dccd13ff12b7a2ab
Natalie Ortiz <ndnatalie@gmail.com>
2016-12-06 13:29:42 -0600

    Add grid

---
+++ app/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+
+@section('title')
+	ICRM
+@stop 
+
+@section('css')
+{{-- 	<link rel="stylesheet" href="/css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="/css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" href="/css/bootstrap.css">
+	<link rel="stylesheet" href="/css/bootstrap.min.css"> --}}
+	<link rel="stylesheet" href="/css/home.css">
+@stop
+
+@section('fonts')
+	<link href="https://fonts.googleapis.com/css?family=Maitree" rel="stylesheet">
+@stop 
+
+@section('content')
+
+	<div class="row">
+		<div class="col-md-12">
+			<div class="spacer">
+				<p>test test test test</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-5">
+			<img class="img-responsive" src="/img/placeholder.png">
+		</div>
+		<div class="col-md-7">
+			<h1>Rita Wilbanks</h1>
+		</div>
+	</div>
+
+
+@stop
